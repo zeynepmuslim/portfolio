96d2a8bcbf290385bd16721bd00b1851e650717c
zeynepmuslim <zeynep.muslim@icloud.com>
2021-11-18 15:24:02 +0300

    apps are done with slots

--- components/app.vue
+++ components/app.vue
@@ -1,21 +1,14 @@
 <template>
   <div>
-    <div class="grid grid-rows-6">
+    <div class="grid grid-rows-7">
       <div class="row-span-2 mx-auto h-20">
-        <div
-          class="h-20 w-20 bg-fourYellow hover:bg-four bg-contain bg-no-repeat"
-        ></div>
+        <slot name="icon"></slot>
       </div>
-      <span class="text-center font-robom text-2xl py-1.5">4 İşlem Oyunu</span>
-      <span class="text-center font-robol row-span-2 py-4 px-2">
-        İlk göz bebeğim, sistemi deneyerek tanımak ve öğrenmek için
-        geliştirdiğim oyunum.
-      </span>
-      <div class="transition duration-500 ease-in-out bg-yellow hover:bg-black group row-start-6 w-1/2 hover:text-yellow rounded-full h-10 mx-auto grid grid-cols-3">
+      <slot name="title"></slot>
+      <slot name="text"></slot>
+      <div class="mt-20 transition duration-500 ease-in-out bg-yellow hover:bg-black group row-end-7 w-1/2 hover:text-yellow rounded-full h-10 mx-auto grid grid-cols-3">
         <div class="transition duration-500 ease-in-out h-6 w-6 my-auto ml-9 bg-apple group-hover:bg-appleYellow bg-contain bg-no-repeat"></div>
-        <span class="font-bebas my-auto col-span-2 text-xl pt-1 pl-2"
-          >App Store</span
-        >
+        <span class="font-bebas my-auto col-span-2 text-xl pt-1 pl-2">App Store</span>
       </div>
     </div>
   </div>
--- components/myApps.vue
+++ components/myApps.vue
@@ -1,11 +1,43 @@
 <template>
   <div class="w-3/4 mx-auto h-96 mt-20">
-  <span class="font-nick text-5xl text-center"><mark>uygulamalarım</mark></span>
-  <div class="grid grid-cols-3 mt-8">
-  <App />
-  <App />
-  <App />
-  </div>
+    <span class="font-nick text-5xl text-center"
+      ><mark>uygulamalarım</mark></span
+    >
+    <div class="grid grid-cols-3 mt-8">
+      <App>
+        <template #text> 
+         <span class="text-center font-robol h-5 row-span-2 py-4 px-2">En çok fonksiyonel ve özelllik barındıran ve büyük çaplı oyunum.</span> 
+        </template>
+        <template #icon>
+          <div class="h-20 w-20 bg-batYellow hover:bg-bat bg-contain bg-no-repeat"></div>
+        </template>
+        <template #title>
+          <span class="text-center font-robom text-2xl py-1.5">Tap and Rise</span>
+        </template>
+      </App>
+      <App>
+        <template #text> 
+         <span class="text-center font-robol h-5 row-span-2 py-4 px-2">Kendi kendime bir challenge olarak belirlediğim 12 saatte sıfırdan uygulama çıkartmak hedefimin ürünü.</span> 
+        </template>
+        <template #icon>
+          <div class="h-20 w-20 bg-pawYellow hover:bg-paw bg-contain bg-no-repeat"></div>
+        </template>
+        <template #title>
+          <span class="text-center font-robom text-2xl py-1.5">Catch Game For Cats</span>
+        </template>
+      </App>
+      <App>
+        <template #text> 
+         <span class="text-center font-robol h-5 row-span-2 py-4 px-2">İlk göz bebeğim, sistemi deneyerek tanımak ve öğrenmek için geliştirdiğim oyunum.</span> 
+        </template>
+        <template #icon>
+          <div class="h-20 w-20 bg-fourYellow hover:bg-four bg-contain bg-no-repeat"></div>
+        </template>
+        <template #title>
+          <span class="text-center font-robom text-2xl py-1.5">4 İşlem Oyunu</span>
+        </template>
+      </App>
+    </div>
   </div>
 </template>
 
@@ -14,7 +46,7 @@ import App from '~/components/App.vue'
 export default {
   name: 'MyApps',
   components: {
-    App
+    App,
   },
 }
 </script>

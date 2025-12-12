dda671b588311183816bab9151f6bd16143a2907
Zeynep Müslim <zeynep.muslim@icloud.com>
2025-12-12 19:58:39 +0300

    package update 4

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,7 @@ export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
   target: 'static',
+  telemetry: false,
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
